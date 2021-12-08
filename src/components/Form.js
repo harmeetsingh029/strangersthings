7d633099769df0c528408e8b0a44631628cb17ed
harmeetsingh029 <singh.h1995@gmail.com>
2021-12-08 12:25:20 -0600

    functioning login and register fields

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,11 +4,31 @@ const Form = () => {
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [usernameRegister, setUsernameRegister] = useState('')
+    const [passwordRegister, setPasswordRegister] = useState('')
+    let isLoggedin = false
 
     async function register(event) {
         event.preventDefault()
-        
-
+        let response = await fetch(
+            'https://strangers-things.herokuapp.com/api/2111-FTB-ET-WEB-FT/users/register',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                user: {
+                  username: usernameRegister,
+                  password: passwordRegister,
+                },
+              }),
+            }
+          )
+        let data = await response.json()
+        setUsernameRegister("")
+        setPasswordRegister("")
+        console.log(data)
     }
   
     async function login(event) {
@@ -30,14 +50,28 @@ const Form = () => {
       )
 
       let user = await response.json()
-    
+      console.log(user)
+      getUser(user.data.token)
+      isLoggedin = true
       setUsername("")
       setPassword("")
     }
   
+    async function getUser(token){
+        let response = await fetch('https://strangers-things.herokuapp.com/api/2111-FTB-ET-WEB-FT/users/me',
+        {
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+      
+        let user = await response.json()
+    }
   
     return (
-      <div id='formContainer'>
+      <div id='loginContainer'>
         <div id='loginContainer'>
         <form onSubmit={login}>
           <label htmlFor='username'>Username:</label>
@@ -48,16 +82,19 @@ const Form = () => {
         </form>
         </div>
 
-
+        {
+           !isLoggedin ? <>
         <div id='registerContainer'>
             <form onSubmit={register}>
-                <label htmlFor='username'>Username:</label>
-                <input type='text' name='username' value={username} onChange={(event) => setUsername(event.target.value)}/>
-                <label htmlFor='password'>Password:</label>
-                <input type='password' name='password' value={password} onChange={(event) => setPassword(event.target.value)}/>
+                <label htmlFor='usernameRegister'>Desired Username:</label>
+                <input type='text' name='usernameRegister' value={usernameRegister} onChange={(event) => setUsernameRegister(event.target.value)}/>
+                <label htmlFor='passwordRegister'>Desired Password:</label>
+                <input type='password' name='passwordRegister' value={passwordRegister} onChange={(event) => setPasswordRegister(event.target.value)}/>
                 <button type='submit'>Register</button>
             </form>
         </div>
+        </> : null
+        }   
       </div>
     )
   }
